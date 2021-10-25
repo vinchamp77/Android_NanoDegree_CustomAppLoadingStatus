42af163405954f25e3518b8cd88880c0cdca70bb
vinchamp77 <vinchamp77@gmail.com>
2021-10-25 13:48:26 +1100

    Implement radio buttons, custom view and button loading animation

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -3,8 +3,11 @@ package com.udacity
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
@@ -13,21 +16,61 @@ class LoadingButton @JvmOverloads constructor(
     private var widthSize = 0
     private var heightSize = 0
 
-    private val valueAnimator = ValueAnimator()
+    private val valueAnimator = ValueAnimator.ofInt(0, 100).setDuration(2000)
 
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+    var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
 
+        when (new) {
+            ButtonState.Loading -> {
+                buttonTextStr = resources.getString(R.string.button_loading)
+
+                valueAnimator.start()
+
+            }
+            ButtonState.Completed -> {
+                buttonTextStr = resources.getString(R.string.button_name)
+                valueAnimator.cancel()
+
+                progress = 0
+            }
+        }
+
+        invalidate()
     }
 
+    private var buttonBackgroundColor = 0
+    private var buttonTextColor = 0
+    private var buttonLoadingColor = 0
+
+    private var buttonTextStr = ""
+    private var progress = 0
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 100.0f
+        typeface = Typeface.create( "", Typeface.BOLD)
+    }
 
     init {
+        // custom attributes
+        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
+            buttonBackgroundColor = getColor(R.styleable.LoadingButton_backgroundColor, 0)
+            buttonTextColor = getColor(R.styleable.LoadingButton_textColor, 0)
+            buttonLoadingColor = getColor(R.styleable.LoadingButton_buttonLoadingColor, 0)
+        }
 
-    }
+        buttonState = ButtonState.Completed
 
-
-    override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
-
+        // setup animation
+        valueAnimator.apply {
+            addUpdateListener {
+                progress = it.animatedValue as Int
+                invalidate()
+            }
+            repeatCount = ValueAnimator.INFINITE
+            repeatMode = ValueAnimator.RESTART
+        }
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -43,4 +86,19 @@ class LoadingButton @JvmOverloads constructor(
         setMeasuredDimension(w, h)
     }
 
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        // button background
+        paint.color = buttonBackgroundColor
+        canvas?.drawRect(0f,0f,widthSize.toFloat(), heightSize.toFloat(), paint)
+
+        // loading button
+        paint.color = buttonLoadingColor
+        canvas?.drawRect(0f, 0f, widthSize * progress/100f, heightSize.toFloat(), paint)
+
+        // text
+        paint.color = buttonTextColor
+        canvas?.drawText(buttonTextStr, widthSize/2.0f, heightSize/2.0f + 30.0f, paint)
+    }
 }
--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -9,11 +9,13 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +25,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
 
+    private var selectedURL: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,24 +35,37 @@ class MainActivity : AppCompatActivity() {
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
         custom_button.setOnClickListener {
-            download()
+
+            if(selectedURL != null) {
+                custom_button.buttonState = ButtonState.Loading
+                download()
+
+            } else {
+                val toast = Toast.makeText(
+                    applicationContext,
+                    getString(R.string.please_select_file),
+                    Toast.LENGTH_LONG)
+                toast.show()
+            }
         }
     }
 
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+
+            custom_button.buttonState = ButtonState.Completed
         }
     }
 
     private fun download() {
-        val request =
-            DownloadManager.Request(Uri.parse(URL))
-                .setTitle(getString(R.string.app_name))
-                .setDescription(getString(R.string.app_description))
-                .setRequiresCharging(false)
-                .setAllowedOverMetered(true)
-                .setAllowedOverRoaming(true)
+
+        val request = DownloadManager.Request(Uri.parse(selectedURL))
+            .setTitle(getString(R.string.app_name))
+            .setDescription(getString(R.string.app_description))
+            .setRequiresCharging(false)
+            .setAllowedOverMetered(true)
+            .setAllowedOverRoaming(true)
 
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
         downloadID =
@@ -56,9 +73,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        private const val URL =
+        private const val GLIDE_URL =
+            "https://github.com/bumptech/glide/archive/refs/heads/master.zip"
+        private const val LOAD_APP_URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val RETROFIT_URL =
+            "https://github.com/square/retrofit/archive/refs/heads/master.zip"
+
         private const val CHANNEL_ID = "channelId"
     }
 
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton && view.isChecked) {
+
+            when (view.getId()) {
+                R.id.radio_glide ->
+                    selectedURL = GLIDE_URL
+                R.id.radio_load_app ->
+                    selectedURL = LOAD_APP_URL
+                R.id.radio_retrofit ->
+                    selectedURL = RETROFIT_URL
+            }
+        }
+    }
 }
